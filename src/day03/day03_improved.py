4ed1fbc838fead94853b28877f3f50facfb328f7
Unknown <kiddid96@gmail.com>
2018-12-05 15:23:08 +0100

    Add day 3

--- src/day03/day03_improved.py
+++ src/day03/day03_improved.py
@@ -19,6 +19,7 @@ def part_1(tot_map, lines, total_size):
     return np.unique(tot_map, return_counts=True)
 
 
+
 def part_2(tot, uni, cnt):
     for i, j in enumerate(uni):
         if tot[j] == cnt[i]:
@@ -27,33 +28,32 @@ def part_2(tot, uni, cnt):
 
 def main():
     # Inputs and setup
-    start_inputs = time.time()
+    start_inputs = time.clock()
 
-    raw_lines = Inputstr("03", test=False)
     line_info = [line.split(" ")[2:] for line in Inputstr("03", test=False).split("\n")]
     area_size = 1000
     total_map = np.zeros((area_size, area_size))
     total_size = [0] * (len(line_info) + 1)
-    print("Setup time: {0:.2f}ms".format((time.time() - start_inputs) * 1000))
+    print("Setup time: {0:.2f}ms".format((time.clock() - start_inputs) * 1000))
 
     # Part 1
-    part_1_start = time.time()  # Timing
+    part_1_start = time.clock()  # Timing
 
     unique, counts = part_1(total_map, line_info, total_size)
     index, = np.where(unique == -1)
     print("\nPart 1:", counts[index][0])
-    print("Time part 2: {0:.2f}ms".format((time.time() - part_1_start) * 1000))
+    print("Time part 2: {0:.2f}ms".format((time.clock() - part_1_start) * 1000))
 
     # Part 2
-    part_2_start = time.time()  # Timing
+    part_2_start = time.clock()  # Timing
 
     unique_int = [int(i) for i in unique]
     non_overlap_id = part_2(total_size, unique_int[2:], counts[2:])
     print("\nPart 2:", non_overlap_id)
-    print("Time part 2: {0:.2f}ms".format((time.time() - part_2_start) * 1000))
+    print("Time part 2: {0:.2f}ms".format((time.clock() - part_2_start) * 1000))
 
 
 if __name__ == "__main__":
-    start_time = time.time()
+    start_time = time.clock()
     main()
-    print("\nTotal time: {0:.2f}ms".format((time.time() - start_time) * 1000))
+    print("\nTotal time: {0:.2f}ms".format((time.clock() - start_time) * 1000))
---
+++ src/day03/day03_test.py
@@ -0,0 +1,54 @@
+import unittest
+import numpy as np
+
+from src.utils import Inputstr
+from src.day03 import day03_improved
+
+
+class Day03TestCase(unittest.TestCase):
+    def setUp_trial(self):
+        self.raw_lines = Inputstr("03", test=True).split("\n")
+        self.line_info = [line.split(" ")[2:] for line in self.raw_lines]
+
+        self.area_size = 1000
+        self.total_map = np.zeros((self.area_size, self.area_size))
+        self.total_size = [0] * (len(self.line_info) + 1)
+
+    def setUp_actual(self):
+        self.raw_lines = Inputstr("03", test=False).split("\n")
+        self.line_info = [line.split(" ")[2:] for line in self.raw_lines]
+
+        self.area_size = 1000
+        self.total_map = np.zeros((self.area_size, self.area_size))
+        self.total_size = [0] * (len(self.line_info) + 1)
+
+    def test_day03_part1_test(self):
+        self.setUp_trial()
+        unique, counts = day03_improved.part_1(self.total_map, self.line_info, self.total_size)
+        index, = np.where(unique == -1)
+        self.assertEqual(counts[index][0], 4)
+
+    def test_part2_test(self):
+        self.setUp_trial()
+        unique, counts = day03_improved.part_1(self.total_map, self.line_info, self.total_size)
+        unique_int = [int(i) for i in unique]
+        non_overlap_id = day03_improved.part_2(self.total_size, unique_int[2:], counts[2:])
+
+        self.assertEqual(non_overlap_id, 3)
+
+    def test_part1_actual(self):
+        self.setUp_actual()
+        unique, counts = day03_improved.part_1(self.total_map, self.line_info, self.total_size)
+        index, = np.where(unique == -1)
+        self.assertEqual(counts[index][0], 100261)
+
+    def test_part2_actual(self):
+        self.setUp_actual()
+        unique, counts = day03_improved.part_1(self.total_map, self.line_info, self.total_size)
+        unique_int = [int(i) for i in unique]
+        non_overlap_id = day03_improved.part_2(self.total_size, unique_int[2:], counts[2:])
+
+        self.assertEqual(non_overlap_id, 251)
+
+if __name__== '__main__':
+    unittest.main()
